be338cecdbe420bc8dec9eca18313ce0b77d10dc
Braden Smelser <bsmelser@odin.cs.csubak.edu>
2024-04-09 13:32:11 -0700

    first commit

---
+++ final.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+
+#define MAX 10
+
+
+using namespace std;
+
+int main(){
+
+cout << "hello world!" << endl;
+
+
+
+
+
+
+
+
+
+    return 0;
+}
