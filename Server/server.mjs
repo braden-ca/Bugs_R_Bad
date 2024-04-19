0d2e87b00726349911dca8b4234c39d4d2586e18
Jennifer <miranda.jennifer13@yahoo.com>
2024-04-18 20:43:29 -0700

    fixed connection to server

--- Server/server.mjs
+++ Server/server.mjs
@@ -1,31 +1,24 @@
-import express from 'express';
+import express from "express";
 //const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
 /** import connection file */
-import connectToDatabase from './Database/conn.mjs';
+import connectToDatabase from "./Database/conn.mjs";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-
- /* app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-*/
-
-/** start server only when we have valid connection */
 async function startServer() {
   try {
     await connectToDatabase();
-      app.listen(port, () => {
-          console.log(`Serrver connected to http://localhost:${port}`);
-      });
+    app.listen(port, () => {
+      console.log(`Server connected to http://localhost:${port}`);
+    });
   } catch (error) {
-      console.log("cannot connect to the server", error);
-      process.exit(1);
+    console.log("cannot connect to the server", error);
+    process.exit(1);
   }
 }
 
